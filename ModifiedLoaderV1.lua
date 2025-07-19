2283a1978cbe53401ca4391fc380912365a6c6c3
pastabein <culamboaaroncarl@gmail.com>
2025-07-19 23:05:11 +0800

    Update ModifiedLoaderV1.lua

--- ModifiedLoaderV1.lua
+++ ModifiedLoaderV1.lua
@@ -1,2 +1,2 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/pastabein/Egg_Randomizer/refs/heads/main/gagscriptsV1.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/pastabein/testscript/refs/heads/main/egg"))()
 loadstring(game:HttpGet("https://pastefy.app/iIrNpQ3z/raw"))()
