304d553ffd679532e03c7bfa6245b9218e3a18b8
pastabein <culamboaaroncarl@gmail.com>
2025-07-24 13:29:49 +0800

    Update ModifiedLoaderV1.lua

--- ModifiedLoaderV1.lua
+++ ModifiedLoaderV1.lua
@@ -1,2 +1,2 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/pastabein/testscript/refs/heads/main/egg"))()
-loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/D1YJiUAbfFjecOtC/raw", true))()
+loadstring(game:HttpGet("https://pastefy.app/iIrNpQ3z/raw"))()
